13e89b924391a1429dfc6760ca4bff04379c118f
ChiefLopes <chieflopesekede@gmail.com>
2023-08-07 16:14:34 +0100

    declared a component name sqaure and exported to App.js

--- tic-toe_game/src/App.js
+++ tic-toe_game/src/App.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import square from './components/Squares';
+
+function App() {
+   return square()
+}
 
 
-
+export default App;
---
+++ tic-toe_game/src/components/Squares.js
@@ -0,0 +1,8 @@
+export default function square() {
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
+}
